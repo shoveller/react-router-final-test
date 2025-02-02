0064b2d69f7b3aa0ea8cabc65252cf58b5e287cc
cinos81 <cinos81@gmail.com>
2025-02-02 12:20:39 +0900

    feat: init

---
+++ src/Detail.tsx
@@ -0,0 +1,11 @@
+const Detail = () => {
+  return (
+    <form>
+      <input type="text" name="user" />
+      <button type="submit">서브밋</button>
+      <button type="reset">리셋</button>
+    </form>
+  );
+};
+
+export default Detail;
